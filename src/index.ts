b78de70c0cbce2c25c103b7eae9f429315ee8cd1
Hossam Ahmed <hoss.ahmed.dev@gmail.com>
2025-09-14 00:05:33 +0300

    session update

--- src/index.ts
+++ src/index.ts
@@ -42,8 +42,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
     },
   })
 );
